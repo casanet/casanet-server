f85826e872f7e5d16279924335f2fa3eff07c7d9
haimkastner <haim.kastner@gmail.com>
2017-12-10 00:18:21 +0200

    bug fix, when turn on and lamp dont have values for brights and whit color

--- modules/Philips/philipsHandler.js
+++ modules/Philips/philipsHandler.js
@@ -5,7 +5,30 @@ const miio = require('miio');
 var updateChangesCallbacks = [];
 
 var ChangeState = (device, state, next) => {
-    YeelightHandler.ChangeState(device, state, next);
+    YeelightHandler.ChangeState(device, state, (err) => {
+        // Some time (i dont know when) the lamp is set bright and color temo to 0
+        // and when turn on the lamp show small light in center of lamp
+        // next code section check it and if so, set default values to lamp
+        if (err || state != 'on')
+            next(err);
+        else {
+            GetBrightness(device, (bright, err) => {
+                GetColorTemperature(device, (white_temp, err) => {
+                    if (bright != 0 && white_temp != 0) {
+                        next(err);
+                    } else {
+                        SetBrightness(device, 50, (err) => {
+                            SetColorTemperature(device, 50, (err) => {
+                                device.bright = 50;
+                                device.white_temp = 50;
+                                next(err);
+                            })
+                        })
+                    }
+                });
+            });
+        }
+    });
 };
 
 var GetState = (device, next) => {
