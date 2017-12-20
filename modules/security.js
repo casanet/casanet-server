3e604b22940e8f014688ecb506b11fc9052df9dd
haimkastner <haim.kastner@gmail.com>
2017-12-20 16:25:22 +0200

    cancel access by ip address, can access only by cookie

--- modules/security.js
+++ modules/security.js
@@ -1,29 +1,10 @@
 // Logger
 var logger = require('./logs');
 
-var fs = require('fs')
 var logs = require('./logs');
 var shortid = require('shortid');
 
-var sessions;
-try {
-  sessions = require('../DB/sessions.json');
-} catch (error) {
-  sessions = {}
-}
-
 var sessionsIdMap = {};
-
-var GetIp = function (req) {
-  var ipAddr = req.headers["x-forwarded-for"];
-  if (ipAddr) {
-    var list = ipAddr.split(",");
-    ipAddr = list[list.length - 1];
-  } else {
-    ipAddr = req.connection.remoteAddress;
-  }
-  return ipAddr;
-}
 
 var GetUserAccess = function (userName, pass) {
   var users = require('../DB/users.json');;// read users file
@@ -33,80 +14,36 @@ var GetUserAccess = function (userName, pass) {
   return false;
 }
 
-var UpdateSession = function (ip, isAccess) {
-  if (!(ip in sessions))
-    sessions[ip] = {};
-
-  sessions[ip].allow = isAccess;
-
-  // Save to session file
-  fs.writeFile('./DB/sessions.json', JSON.stringify(sessions), 'utf-8', function (err) {
-    if (err)
-      logger.write.error('Error to write session file');
-    else
-      logger.write.debug('Done to update session file');
-  })
-}
-
-var CheckSession = function (ip) {
-  var checkResult = typeof (ip) !== "undefined" &&
-    ip &&
-    ip in sessions &&
-    sessions[ip].allow;
-
-  return checkResult;
-}
-
-
 var CheckIn = function (req, res, useName, pass, next) {
-  ip = GetIp(req);
   isAccess = GetUserAccess(useName, pass);
-
-  UpdateSession(ip, isAccess);
 
   if (isAccess) {
     var sessionId = shortid.generate()
     sessionsIdMap[sessionId] = true;
-    res.cookie('sessionID' , sessionId, {maxAge : 4.32e+8}); // 5 days
+    res.cookie('sessionID', sessionId, { maxAge: 4.32e+8 }); // 5 days
   }
   next(isAccess);
 }
 
 var CheckOut = function (req, res) {
-  ip = GetIp(req);
   var sessionCookie = req.cookies.sessionID;
   delete sessionsIdMap[sessionCookie];
   res.cookie('sessionID', 'empty');
-  UpdateSession(ip, false);
 }
 
 var CheckAccess = function (req, res, next) {
   var sessionCookie = req.cookies.sessionID;
   if (sessionCookie in sessionsIdMap) {
+    res.cookie('sessionID', sessionCookie, { maxAge: 4.32e+8 }); // 5 days    
     next();
-    return;
-  }
-
-  ip = GetIp(req);
-  if (!CheckSession(ip)) {
+  } else {
     res.statusCode = 403;
     res.send('Athontication error');
-  } else {
-    next();
   }
 }
 
 var ClearCache = (callback) => {
-  sessions = {};
   sessionsIdMap = {};
-  fs.writeFile('./DB/sessions.json', JSON.stringify(sessions), 'utf-8', function (err) {
-    if (err)
-      logger.write.error('Error to write session file');
-    else
-      logger.write.debug('Done to update session file');
-
-    callback(err);
-  })
 }
 
 module.exports = {
