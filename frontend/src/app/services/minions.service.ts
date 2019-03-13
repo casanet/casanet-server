ebd5da0791e869fd845ab3b8d5bc7b561737d243
haim kastner <haim.kastner@gmail.com>
2019-03-13 14:25:38 +0200

    Implementation of minion delete.

--- frontend/src/app/services/minions.service.ts
+++ frontend/src/app/services/minions.service.ts
@@ -6,174 +6,185 @@ import { DeepCopy } from '../../../../backend/src/utilities/deepCopy';
 import { ToasterAndErrorsService } from './toaster-and-errors.service';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class MinionsService {
+	private isMinionsRetrived = false;
+	private minionsServerFeed: EventSource;
+	private minions: Minion[] = [];
+	public minionsFeed: BehaviorSubject<Minion[]> = new BehaviorSubject<Minion[]>(this.minions);
 
-  private isMinionsRetrived = false;
-  private minionsServerFeed: EventSource;
-  private minions: Minion[] = [];
-  public minionsFeed: BehaviorSubject<Minion[]> = new BehaviorSubject<Minion[]>(this.minions);
+	constructor(private toastrAndErrorsService: ToasterAndErrorsService, private httpClient: HttpClient) {}
 
-  constructor(private toastrAndErrorsService: ToasterAndErrorsService,
-    private httpClient: HttpClient) {
+	public async recordCommand(minion: Minion, minionStatus: MinionStatus) {
+		try {
+			await this.httpClient.post(`/API/minions/command/${minion.minionId}`, minionStatus).toPromise();
+		} catch (error) {
+			this.toastrAndErrorsService.OnHttpError(error);
+		}
+	}
 
-  }
+	public async refreshMinions() {
+		if (this.minionsServerFeed) {
+			this.minionsServerFeed.close();
+		}
 
-  public async recordCommand(minion: Minion, minionStatus: MinionStatus) {
-    try {
-      await this.httpClient.post(`/API/minions/command/${minion.minionId}`, minionStatus).toPromise();
-    } catch (error) {
-      this.toastrAndErrorsService.OnHttpError(error);
-    }
-  }
+		this.minions = [];
+		try {
+			await this.httpClient.post(`/API/minions/rescan`, {}).toPromise();
+			await this.loadMinions();
+		} catch (error) {
+			this.toastrAndErrorsService.OnHttpError(error);
+		}
+	}
 
-  public async refreshMinions() {
-    if (this.minionsServerFeed) {
-      this.minionsServerFeed.close();
-    }
+	public async refreshMinion(minion: Minion) {
+		try {
+			await this.httpClient.post(`/API/minions/rescan/${minion.minionId}`, {}).toPromise();
+		} catch (error) {
+			this.toastrAndErrorsService.OnHttpError(error);
+		}
+	}
 
-    this.minions = [];
-    try {
-      await this.httpClient.post(`/API/minions/rescan`, {}).toPromise();
-      await this.loadMinions();
+	private async loadMinions() {
+		try {
+			const minions = await this.httpClient.get<Minion[]>('/API/minions').toPromise();
+			this.isMinionsRetrived = true;
+			this.minions = minions;
 
-    } catch (error) {
-      this.toastrAndErrorsService.OnHttpError(error);
-    }
-  }
+			for (const minion of this.minions) {
+				this.loadDefaultStatusValues(minion);
+			}
 
-  public async refreshMinion(minion: Minion) {
-    try {
-      await this.httpClient.post(`/API/minions/rescan/${minion.minionId}`, {}).toPromise();
-    } catch (error) {
-      this.toastrAndErrorsService.OnHttpError(error);
-    }
-  }
+			this.minionsFeed.next(DeepCopy<Minion[]>(this.minions));
+		} catch (error) {
+			this.isMinionsRetrived = false;
+			this.toastrAndErrorsService.OnHttpError(error);
+		}
 
-  private async loadMinions() {
-    try {
-      const minions = await this.httpClient.get<Minion[]>('/API/minions').toPromise();
-      this.isMinionsRetrived = true;
-      this.minions = minions;
+		if (this.minionsServerFeed) {
+			this.minionsServerFeed.close();
+		}
 
-      for (const minion of this.minions) {
-        this.loadDefaultStatusValues(minion);
-      }
+		this.minionsServerFeed = new EventSource('/API/feed/minions');
+		this.minionsServerFeed.onmessage = (minionFeedEvent: MessageEvent) => {
+			this.OnMinionFeedUpdate(minionFeedEvent);
+		};
+	}
 
-      this.minionsFeed.next(DeepCopy<Minion[]>(this.minions));
-    } catch (error) {
-      this.isMinionsRetrived = false;
-      this.toastrAndErrorsService.OnHttpError(error);
-    }
+	private loadDefaultStatusValues(minion: Minion) {
+		if (
+			!minion.minionStatus[minion.minionType] ||
+			JSON.stringify(minion.minionStatus[minion.minionType]) === '{}'
+		) {
+			minion.minionStatus = {
+				airConditioning: {
+					fanStrength: 'auto',
+					mode: 'auto',
+					status: 'off',
+					temperature: 16
+				},
+				colorLight: {
+					blue: 1,
+					brightness: 1,
+					green: 1,
+					red: 1,
+					status: 'off',
+					temperature: 1
+				},
+				light: {
+					brightness: 1,
+					status: 'off'
+				},
+				switch: {
+					status: 'off'
+				},
+				temperatureLight: {
+					brightness: 1,
+					status: 'off',
+					temperature: 1
+				},
+				toggle: {
+					status: 'off'
+				}
+			};
+		}
+	}
 
+	public async retriveMinions() {
+		if (!this.isMinionsRetrived) {
+			this.loadMinions();
+		}
+	}
 
-    if (this.minionsServerFeed) {
-      this.minionsServerFeed.close();
-    }
+	private OnMinionFeedUpdate(minionFeedEvent: MessageEvent) {
+		if (minionFeedEvent.lastEventId === '0') {
+			return;
+		}
 
-    this.minionsServerFeed = new EventSource('/API/feed/minions');
-    this.minionsServerFeed.onmessage = (minionFeedEvent: MessageEvent) => { this.OnMinionFeedUpdate(minionFeedEvent); };
-  }
+		const minionFeed: MinionFeed = JSON.parse(minionFeedEvent.data);
 
-  private loadDefaultStatusValues(minion: Minion) {
-    if (!minion.minionStatus[minion.minionType] || JSON.stringify(minion.minionStatus[minion.minionType]) === '{}') {
-      minion.minionStatus = {
-        airConditioning: {
-          fanStrength: 'auto',
-          mode: 'auto',
-          status: 'off',
-          temperature: 16,
-        },
-        colorLight: {
-          blue: 1,
-          brightness: 1,
-          green: 1,
-          red: 1,
-          status: 'off',
-          temperature: 1,
-        },
-        light: {
-          brightness: 1,
-          status: 'off',
-        },
-        switch: {
-          status: 'off',
-        },
-        temperatureLight: {
-          brightness: 1,
-          status: 'off',
-          temperature: 1,
-        },
-        toggle: {
-          status: 'off',
-        }
-      };
-    }
-  }
+		switch (minionFeed.event) {
+			case 'update': {
+				this.loadDefaultStatusValues(minionFeed.minion);
+				const minion = this.findMinion(minionFeed.minion.minionId);
+				if (minion) {
+					this.minions.splice(this.minions.indexOf(minion), 1);
+				}
+				this.minions.push(minionFeed.minion);
+				break;
+			}
+			case 'created': {
+				this.loadDefaultStatusValues(minionFeed.minion);
+				this.minions.push(minionFeed.minion);
+				break;
+			}
+			case 'removed': {
+				const minion = this.findMinion(minionFeed.minion.minionId);
+				if (minion) {
+					this.minions.splice(this.minions.indexOf(minion), 1);
+				}
+				break;
+			}
+		}
 
-  public async retriveMinions() {
-    if (!this.isMinionsRetrived) {
-      this.loadMinions();
-    }
-  }
+		this.minionsFeed.next(DeepCopy<Minion[]>(this.minions));
+	}
 
-  private OnMinionFeedUpdate(minionFeedEvent: MessageEvent) {
-    if (minionFeedEvent.lastEventId === '0') {
-      return;
-    }
+	private findMinion(minoinId: string): Minion {
+		for (const minion of this.minions) {
+			if (minion.minionId === minoinId) {
+				return minion;
+			}
+		}
+	}
 
-    const minionFeed: MinionFeed = JSON.parse(minionFeedEvent.data);
+	public async setStatus(setMinion: Minion) {
+		const minion = this.findMinion(setMinion.minionId);
+		try {
+			await this.httpClient.put(`/API/minions/${setMinion.minionId}`, setMinion.minionStatus).toPromise();
 
-    switch (minionFeed.event) {
-      case 'update': {
-        this.loadDefaultStatusValues(minionFeed.minion);
-        const minion = this.findMinion(minionFeed.minion.minionId);
-        if (minion) {
-          this.minions.splice(this.minions.indexOf(minion), 1);
-        }
-        this.minions.push(minionFeed.minion);
-        break;
-      }
-      case 'created': {
-        this.loadDefaultStatusValues(minionFeed.minion);
-        this.minions.push(minionFeed.minion);
-        break;
-      }
-      case 'removed': {
-        const minion = this.findMinion(minionFeed.minion.minionId);
-        if (minion) {
-          this.minions.splice(this.minions.indexOf(minion), 1);
-        }
-        break;
-      }
-    }
+			minion.isProperlyCommunicated = true;
+			minion.minionStatus = DeepCopy<MinionStatus>(setMinion.minionStatus);
+		} catch (error) {
+			minion.isProperlyCommunicated = false;
+			this.toastrAndErrorsService.OnHttpError(error);
+		}
+		this.minionsFeed.next(DeepCopy<Minion[]>(this.minions));
+	}
 
-    this.minionsFeed.next(DeepCopy<Minion[]>(this.minions));
-  }
+	public async deleteStatus(minionToRemove: Minion) {
+		try {
+			await this.httpClient.delete(`/API/minions/${minionToRemove.minionId}`).toPromise();
 
-  private findMinion(minoinId: string): Minion {
-    for (const minion of this.minions) {
-      if (minion.minionId === minoinId) {
-        return minion;
-      }
-    }
-  }
+			const minion = this.findMinion(minionToRemove.minionId);
+			if (minion) {
+				this.minions.splice(this.minions.indexOf(minion), 1);
+			}
 
-  public async setStatus(setMinion: Minion) {
-    const minion = this.findMinion(setMinion.minionId);
-    try {
-      await this.httpClient.put(`/API/minions/${setMinion.minionId}`, setMinion.minionStatus).toPromise();
-
-      minion.isProperlyCommunicated = true;
-      minion.minionStatus = DeepCopy<MinionStatus>(setMinion.minionStatus);
-
-    } catch (error) {
-      minion.isProperlyCommunicated = false;
-      this.toastrAndErrorsService.OnHttpError(error);
-    }
-    this.minionsFeed.next(DeepCopy<Minion[]>(this.minions));
-  }
-
-
+			this.minionsFeed.next(DeepCopy<Minion[]>(this.minions));
+		} catch (error) {
+			this.toastrAndErrorsService.OnHttpError(error);
+		}
+	}
 }
