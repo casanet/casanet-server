22885aff323c27bd2f43642d64f83cef4b33be4b
Haim Kastnet <haim.kastner@gmail.com>
2019-03-20 16:08:19 +0200

    Easier copy-paste code from MFA email.

--- backend/src/utilities/mailSender.ts
+++ backend/src/utilities/mailSender.ts
@@ -26,7 +26,7 @@ export const SendMail = async (to: string, code: string) => {
         replyTo: undefined,
         inReplyTo: undefined,
         subject: 'CASAnet authentication code',
-        html: `<div> Your authentication code is: <br><br><b> ${code} </b><br><br> The password valid only in next 5 minutes. </div>`,
+        html: `<div> Your authentication code is: <br><textarea readonly>${code}</textarea><br>The password valid only in next 5 minutes. </div>`,
     };
 
     // send mail with defined transport object
