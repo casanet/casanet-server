7982ffd1fd79c62c0b20a15bb19c477189d4b9e7
Haim Kastnet <haim.kastner@gmail.com>
2019-04-19 00:59:14 +0300

    Ifttt as toggle. #10. - now can sent on/off commands only.

---
+++ backend/src/modules/ifttt/iftttHandler.ts
@@ -0,0 +1,66 @@
+import * as moment from 'moment';
+import { Duration } from 'moment';
+import * as request from 'request-promise';
+import { BehaviorSubject } from 'rxjs';
+import { DeviceKind, ErrorResponse, Minion, MinionStatus, SwitchOptions, Toggle } from '../../models/sharedInterfaces';
+import { DeepCopy } from '../../utilities/deepCopy';
+import { logger } from '../../utilities/logger';
+import { BrandModuleBase } from '../brandModuleBase';
+
+export class IftttHandler extends BrandModuleBase {
+
+    public readonly brandName: string = 'ifttt';
+
+    public readonly devices: DeviceKind[] = [
+        {
+            brand: this.brandName,
+            isTokenRequierd: false,
+            isIdRequierd: true,
+            minionsPerDevice: -1,
+            model: 'toggle',
+            suppotedMinionType: 'toggle',
+            isRecordingSupported: false,
+        },
+    ];
+
+    constructor() {
+
+        super();
+    }
+    public async getStatus(miniom: Minion): Promise<MinionStatus | ErrorResponse> {
+        return {
+            status: 'on',
+        } as MinionStatus;
+    }
+
+    public async setStatus(miniom: Minion, setStatus: MinionStatus): Promise<void | ErrorResponse> {
+        try {
+            // tslint:disable-next-line:max-line-length
+            await request(`https://maker.ifttt.com/trigger/${miniom.minionId}-${setStatus[miniom.minionType].status}/with/key/${miniom.device.deviceId}`);
+        } catch (error) {
+            logger.warn(`Sent IFTTT trigger for ${miniom.minionId} fail, ${JSON.stringify(error.message)}`);
+            throw {
+                responseCode: 7409,
+                message: 'Ifttt triggger fail.',
+            } as ErrorResponse;
+        }
+    }
+
+    public async enterRecordMode(miniom: Minion, statusToRecordFor: MinionStatus): Promise<void | ErrorResponse> {
+        throw {
+            responseCode: 6409,
+            message: 'the ifttt module not support any recording mode',
+        } as ErrorResponse;
+    }
+
+    public async generateCommand(miniom: Minion, statusToRecordFor: MinionStatus): Promise<void | ErrorResponse> {
+        throw {
+            responseCode: 6409,
+            message: 'the ifttt module not support any recording mode',
+        } as ErrorResponse;
+    }
+
+    public async refreshCommunication(): Promise<void> {
+        // There's nothing to do.
+    }
+}
