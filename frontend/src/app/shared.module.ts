9e253450f4e6ab0ae06e660b5906575cc7bbe59b
haim kastner <haim.kastner@gmail.com>
2020-04-17 10:04:15 +0300

    Add hidden music component Can show only manully by setting "musicFrameUrl" in the local storage

--- frontend/src/app/shared.module.ts
+++ frontend/src/app/shared.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { TranslatePipe } from './translate.pipe';
 import { TimesPipe } from './pipes/times.pipe';
+import { SafePipe } from './pipes/safe.pipe';
 import { LoaderComponent } from './dashboard-crm/loader/loader.component';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [TranslatePipe, TimesPipe, LoaderComponent],
-    exports: [TranslatePipe, TimesPipe, LoaderComponent, MatProgressBarModule]
+    declarations: [TranslatePipe, TimesPipe, SafePipe, LoaderComponent],
+    exports: [TranslatePipe, TimesPipe, SafePipe, LoaderComponent, MatProgressBarModule]
 })
 export class SharedModule { }
