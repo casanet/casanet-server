0ee6a0dd90ee9b10364a240cfcbeea85eee95fec
Haim Kastner <haim.kastner@gmail.com>
2021-09-14 11:09:24 +0300

    Fix v3 dashboard serving

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -63,14 +63,21 @@ class App {
 
 		/** Get any file in public directory */
 		this.express.use(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-			const filePath = path.join(__dirname, '/public/', req.url);
-			console.log(`CHECKING PUBLIC: "${filePath}"`);
+
+			// The v3 dashboard assets placed in the public dir, so redirect thr V3 requests to there.
+			let url = req.url || '';
+			if (url.startsWith('v3') || url.startsWith('/v3')) {
+				url = url.replace('v3', 'public');
+			} else {
+				next();
+				return;
+			}
+
+			const filePath = path.join(__dirname, url);
 			fse.exists(filePath, exists => {
-				console.log(`EXISTS PUBLIC: "${filePath}"`);
 				if (exists) {
 					res.sendFile(filePath);
 				} else {
-					console.log(`404 PUBLIC: "${filePath}"`);
 					next();
 				}
 			});
@@ -89,13 +96,10 @@ class App {
 		/** Get any file in public directory */
 		this.express.use(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
 			const filePath = path.join(__dirname, '/dashboard/', req.url);
-			console.log(`CHECKING DASHBOARD: "${filePath}"`);
 			fse.exists(filePath, exists => {
 				if (exists) {
-					console.log(`EXISTS DASHBOARD: "${filePath}"`);
 					res.sendFile(filePath);
 				} else {
-					console.log(`404 DASHBOARD: "${filePath}"`);
 					next();
 				}
 			});
