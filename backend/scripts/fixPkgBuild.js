324507bafaaccf31504907f47a7aee173e6292d6
Haim Kastner <haim.kastner@gmail.com>
2021-09-16 21:06:46 +0300

    use v14 in armv node

--- backend/scripts/fixPkgBuild.js
+++ backend/scripts/fixPkgBuild.js
@@ -3,9 +3,13 @@ import fse from 'fs-extra';
 
 // See https://github.com/vercel/ncc/issues/521 https://github.com/vercel/pkg/issues/943 https://github.com/ljharb/es-get-iterator/issues/5
 (async () => {
-	const packagePath = path.join('node_modules', 'es-get-iterator', 'package.json');
-	const packageManifest = await fse.readJSON(packagePath);
+	let packagePath = path.join('node_modules', 'es-get-iterator', 'package.json');
+	let packageManifest = await fse.readJSON(packagePath);
 	packageManifest.main = 'mode.js';
 	delete packageManifest.exports;
 	fse.writeJSON(packagePath, packageManifest);
+
+	packagePath = path.join('node_modules', 'nanoio', 'package.json');
+	packageManifest = await fse.readJSON(packagePath);
+	delete packageManifest.exports;
 })();
