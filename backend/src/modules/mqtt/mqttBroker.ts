0f6463bb60fc1904d00e6057ec10374a2be3688c
Haim Kastner <haim.kastner@gmail.com>
2021-09-17 01:06:15 +0300

    Shift from mosca to aedes MQTT server

--- backend/src/modules/mqtt/mqttBroker.ts
+++ backend/src/modules/mqtt/mqttBroker.ts
@@ -1,10 +1,12 @@
 import * as ip from 'ip';
-import * as mosca from 'mosca';
+import * as aedes from 'aedes';
+import { createServer } from 'aedes-server-factory';
 import { logger } from '../../utilities/logger';
+import { Server } from 'net';
 
 /** Simple Mqtt server */
 export class MqttBroker {
-  private server: mosca.Server;
+  private server: Server;
 
   constructor() {}
 
@@ -14,17 +16,20 @@ export class MqttBroker {
    * @returns  The broker ip.
    */
   public async invokeBroker(port: number): Promise<string> {
-    this.server = new mosca.Server({
-      port,
-    });
-
-    this.server.on('ready', () => {
-      logger.info(`Mosca mqtt server on ${ip.address()}:${port} is up and running`);
-    });
-
-    this.server.on('clientConnected', client => {
-      logger.info(`Mqtt ${client.id} client connected`);
-    });
+		this.server = createServer(aedes.Server());
+		
+		this.server.listen(port, () => {
+      logger.info(`[MqttBroker] aedes mqtt server on ${ip.address()}:${port} is up and running`);
+		});
+		this.server.on('close' ,() => {
+      logger.warn(`[MqttBroker] aedes mqtt server  ${ip.address()}:${port} is closed`);
+		});
+		this.server.on('connection' ,() => {
+      logger.warn(`[MqttBroker] aedes mqtt server  ${ip.address()}:${port} connection arrived`);
+		});
+		this.server.on('error' ,() => {
+      logger.warn(`[MqttBroker] aedes mqtt server  ${ip.address()}:${port} got error`);
+		});
 
     return ip.address();
   }
