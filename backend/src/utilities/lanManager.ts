b36f9040af17fdfc5bc0526b9a529df9d8449a7b
Haim Kastner <haim.kastner@gmail.com>
2021-10-02 22:36:49 +0300

    Shift to faster local-network-scan

--- backend/src/utilities/lanManager.ts
+++ backend/src/utilities/lanManager.ts
@@ -1,60 +1,47 @@
 import * as ip from 'ip';
 import * as isOnline from 'is-online';
-import * as networkList2 from 'network-list2';
 import { Configuration } from '../config';
 import { LocalNetworkDevice } from '../models/sharedInterfaces';
 import { logger } from './logger';
-import { GetMachinMacAddress } from './macAddress';
+import { scanLocalNetwork } from 'local-network-scan';
 
 /**
- * Get the all local network devices.
+ * Scan local network devices 
+ * @returns LocalNetworkDevice collection
  */
-export const LocalNetworkReader = (): Promise<LocalNetworkDevice[]> => {
-  logger.info('Scanning network devices...');
-  return new Promise(async (resolve, reject) => {
-    const ops: { ip?: string; vendor?: boolean } = {};
+export async function LocalNetworkReader(): Promise<LocalNetworkDevice[]> {
 
-    /** Check if internet connection is online, otherwise don't try to get vendor name. */
-    const isInternetOnline = await isOnline();
+	/** Check if internet connection is online, otherwise don't try to get vendor name. */
+	const isInternetOnline = await isOnline();
 
-    if (Configuration.scanSubnet) {
-      ops.ip = Configuration.scanSubnet;
-      ops.vendor = isInternetOnline;
-    }
+	if (Configuration.runningMode === 'test') {
+		return [];
+	}
 
-    if(Configuration.runningMode === 'test'){
-      resolve([]);
-      return 
-    }
+	try {
+		logger.info('[LocalNetworkReader] Scanning network devices...');
+		const networkDevices = await scanLocalNetwork({ localNetwork: Configuration.scanSubnet, queryVendor: isInternetOnline });
+		logger.info('[LocalNetworkReader] Scanning network devices done.');
 
-    networkList2.scan(ops, (err: any, netTableArray: any[]) => {
-      logger.info('Scanning network devices done.');
-      if (err) {
-        const msg = 'Scen local network fail';
-        logger.warn(msg);
-        reject(msg);
-        return;
-      }
+		const devices: LocalNetworkDevice[] = [];
+		/** Add current machine info to table (without the MAC address!!!, then mac used as part of the default authentication) */
+		devices.push({
+			mac: '------------',
+			ip: ip.address(),
+			vendor: 'casa-net server',
+		});
 
-      const devices: LocalNetworkDevice[] = [];
-
-      /** Add current machine info to table (without the MAC address!!!, then mac used as part of the default authentication) */
-      devices.push({
-        mac: '------------',
-        ip: ip.address(),
-        vendor: 'casa-net server',
-      });
-
-      for (const localDevice of netTableArray) {
-        if (localDevice.alive && localDevice.mac) {
-          devices.push({
-            mac: localDevice.mac.replace(/:|-|_| /g, '').toLowerCase(),
-            ip: localDevice.ip,
-            vendor: localDevice.vendor ? localDevice.vendor : undefined,
-          });
-        }
-      }
-      resolve(devices);
-    });
-  });
-};
+		for (const localDevice of networkDevices) {
+			devices.push({
+				// Show clean MAC string without ':', '-' or '_'
+				mac: localDevice.mac.replace(/:|-|_| /g, '').toLowerCase(),
+				ip: localDevice.ip,
+				vendor: localDevice.vendor,
+			});
+		}
+		return devices;
+	} catch (error) {
+		logger.error(`[LocalNetworkReader] Scanning network devices failed - ${error.message}`);
+		return [];
+	}
+}
